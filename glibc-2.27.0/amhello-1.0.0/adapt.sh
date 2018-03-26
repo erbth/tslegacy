2fda5230b818efdabb262293e85cb0704d246d8d
erbth <t.erbesdobler@team103.com>
2018-03-26 19:52:12 +0200

    Ongoing cleanup

--- glibc-2.27.0/amhello-1.0.0/adapt.sh
+++
@@ -1,4 +0,0 @@
-# No shebang here because I don't want to depend on bash being installed in a
-# specific location.
-
-echo "Hi from adapt.sh!"
--- glibc-2.27.0/update_playground.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -r Makefile adjust_toolchain.sh restore_toolchain.sh create_tool_links.sh set_env.sample \
-    amhello-1.0.0 \
-    basic_fhs-3.0.0 \
-    iproute2-4.15.0 \
-    playground
