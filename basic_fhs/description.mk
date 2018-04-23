77eaded42b7c5aed4eb709feacfcfd68a5a9fb41
erbth <t.erbesdobler@team103.com>
2018-04-23 23:28:40 +0200

    Now, I can build basic_fhs

--- basic_fhs/description.mk
+++ basic_fhs/description.mk
@@ -1,3 +1,8 @@
+# Information about the source package itself
+basic_fhs_SRC_VERSION := 3.0
+basic_fhs_SRC_ARCHIVE := basic_fhs-$(basic_fhs_SRC_VERSION).tar.xz
+basic_fhs_SRC_DIR := basic_fhs-$(basic_fhs_SRC_VERSION)
+
 # Information about the differrent packages that are built out of this source
 # package.
 basic_fhs_TSL_TYPE := sw
@@ -5,9 +10,10 @@ basic_fhs_TSL_RDEPS :=
 basic_fhs_TSL_CDEPS :=
 basic_fhs_TSL_SRC_PKG := basic_fhs
 
-# Information about the source package itself
-basic_fhs_SRC_VERSION := 3.0
-basic_fhs_SRC_ARCHIVE := basic_fhs-$(basic_fhs_SRC_VERSION).tar.xz
+basic_fhs-dev_TSL_TYPE := sw
+basic_fhs-dev_TSL_RDEPS = basic_fhs=$(call read_version,basic_fhs)
+basic_fhs-dev_TSL_CDEPS :=
+basic_fhs-dev_TSL_SRC_PKG := basic_fhs
 
 # A list of all the packages that are built out of this source package
-basic_fhs_TSL_PKGS := basic_fhs
+basic_fhs_TSL_PKGS := basic_fhs basic_fhs-dev
---
+++ basic_fhs/install_split.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+rm -rvf ${PACKAGING_LOCATION}/basic_fhs{,-dev}/${DESTDIR}/*
+
+# Install to the 'normal' package
+cd ${BUILD_DIR}/${SRC_DIR}
+make ROOT=${PACKAGING_LOCATION}/basic_fhs/${DESTDIR} install
+
+# Move documentation to the dev package
+install -dvm755 ${PACKAGING_LOCATION}/basic_fhs-dev/${DESTDIR}/usr/share/doc
+
+mv -v ${PACKAGING_LOCATION}/basic_fhs/${DESTDIR}/usr/share/doc/basic_fhs \
+    ${PACKAGING_LOCATION}/basic_fhs-dev/${DESTDIR}/usr/share/doc/basic_fhs-dev
