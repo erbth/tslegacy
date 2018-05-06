97febbd20b08319655d2270f2dc912c85ffc65c2
erbth <t.erbesdobler@team103.com>
2018-05-06 13:30:27 +0200

    bzip2 adaption

--- old_pkg_cdep_tree.mk
+++ old_pkg_cdep_tree.mk
@@ -106,12 +106,13 @@
 # $(call built_of_normal_pkg,gdbm): coreutils_installed
 # $(call built_of_normal_pkg,libffi): coreutils_installed
 # $(call built_of_normal_pkg,expat): coreutils_installed
+# $(call built_of_normal_pkg,tslegacy-bootscripts): coreutils_installed \
+#	sed_installed grep_installed findutils_installed bash_installed
 # $(call built_of_normal_pkg,findutils): coreutils_installed
 # $(call built_of_normal_pkg,grep): coreutils_installed
 # $(call built_of_normal_pkg,sed): coreutils_installed
 # $(call built_of_normal_pkg,tar): coreutils_installed
 # $(call built_of_normal_pkg,gzip): coreutils_installed
-# $(call built_of_normal_pkg,tslegacy-bootscripts): coreutils_installed
 $(call built_of_normal_pkg,elfutils): coreutils_installed
 $(call built_of_normal_pkg,util-linux): eudev_installed ncurses_installed
 $(call built_of_normal_pkg,eudev): coreutils_installed \
