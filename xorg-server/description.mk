642152fe29464a28a7e40d39cd723280693569d3
erbth <t.erbesdobler@team103.com>
2018-05-26 13:36:30 +0200

    xorg-server defined

---
+++ xorg-server/description.mk
@@ -0,0 +1,32 @@
+ifndef xorg-server_description_included
+xorg-server_description_included := 1
+
+include ${PACKAGING_RESOURCE_DIR}/makefile_utilities.mk
+
+# I took the important compiletime dependencies from the book
+# `Beyond Linux From Scratch', `Version 8.2' by the BLFS Development
+# Team. At the time I initially wrote this file, the book was available
+# from www.linuxfromscratch.org/blfs.
+xorg-server_SRC_VERSION := 1.20.0
+xorg-server_SRC_DIR := xorg-server-$(xorg-server_SRC_VERSION)
+xorg-server_SRC_ARCHIVE := $(xorg-server_SRC_DIR).tar.bz2
+xorg-server_SRC_CDEPS := \
+	gcc_installed \
+	glibc-dev_installed \
+	pixman-dev_installed \
+	xorg-fonts-dev_installed \
+	xkeyboard-config-dev_installed \
+	libepxoy-dev_installed
+
+xorg-server_TSL_TYPE := sw
+xorg-server_TSL_RDEPS =
+xorg-server_TSL_SRC_PKG := xorg-server
+
+xorg-server-dev_TSL_TYPE := sw
+xorg-server-dev_TSL_RDEPS = \
+	$(call equal_dep,xorg-server)
+xorg-server-dev_TSL_SRC_PKG := xorg-server
+
+xorg-server_TSL_PKGS := xorg-server-dev xorg-server
+
+endif
